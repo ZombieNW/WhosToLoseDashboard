04fb5b01fbd5f82b18e44b5b6527d4fd21ce9033
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-01-15 19:41:03 -0700

    Revised contestant agreement

--- src/lib/components/contestant_agreement.svelte
+++ src/lib/components/contestant_agreement.svelte
@@ -1,48 +1,51 @@
 <div class="space-y-2">
 	<h1 class="text-2xl font-semibold text-gray-800">Welcome to Who's to Lose!</h1>
 	<p>
-		Your job is to sell a given story, it may be true, or it may be false! Whether or not people
-		believe you is in your hands.
+		Your job is to easy: sell a story. <br /> That story might be true, or it may be false!
+		<br />Whether or not anyone believes you is your problem.
 	</p>
 	<h2 class="text-xl font-semibold text-gray-800">Game Summary</h2>
 	<p class="md:indent-12">
-		In this game, players take turns confidently telling embarrassing stories prompted by the host
-		regardless of if they are true or not. Other contestants will interrogate their story to
-		determine if they are lying. Storytellers win by tricking, interrogators win by detecting. May
-		the best embarrassing liar win!
+		In <span class="italic">Who's to Lose</span>, players take turns confidently telling
+		embarrassing stories chosen by the host, regardless of if they are true or not. The other
+		contestants will interrogate the storyteller to determine if they're lying. Storytellers win by
+		convincing people. Interrogators win by detecting lies. May the most best embarrassing liar win!
 	</p>
 	<h2 class="text-xl font-semibold text-gray-800">Story Submission</h2>
 	<p class="md:indent-12">
 		An important part of the game is pre-submitted stories by the contestants. After submitting this
-		agreement, please go to the story submission section of the "Todo" and follow the instructions.
-		By submitting stories, you consent to them being used in the show. However, this consent is
-		reversible, <strong>as consent should be.</strong>
+		agreement, go to the Story Submission section and follow the instructions. By submitting
+		stories, you consent to them being used in the show. This consent is reversible.
+		<strong>Because consent should be.</strong>
+	</p>
+	<p class="md:indent-12">
+		One submission per story; Don't be a yapper, write just enough to hook people and give the host
+		something to work with. More submissions -> higher chance to be on the show.
 	</p>
 	<h2 class="text-xl font-semibold text-gray-800">Storytelling</h2>
 	<p class="md:indent-12">
-		As a contestant, you will be prompted to tell a story that may be true or false; the host will
-		simply make a claim about you. In any case, you must treat the admission as true, no matter
-		what. The other contestants will then ask you questions about your story/experience. If it's a
-		true story, good! This should be easy. If it's fake, mix confidence and confusion to make it
-		sound like it really happened. Important Rule: Be funny! Commit to the bit. It's more fun, makes
-		you look more convincing, and makes the video more fun.
+		When it's your turn, the host will make a claim about you. True or false, you must treat the
+		claim as 100% true. Other contestants will then interrogate you about your "experience." If it's
+		true: great! This should be easy. If it's fake: mix confidence and confusion until it sounds
+		real. Important Rule: Be funny. Commit to the bit. It's more fun, makes you more convincing, and
+		makes the video better.
 	</p>
 	<h2 class="text-xl font-semibold text-gray-800">Interrogating</h2>
 	<p class="md:indent-12">
-		When other players are telling stories, it's your turn to ask questions. Give room to other
-		contestants to ask questions about the stories; don't be an attention whore. Respect people's
-		boundaries, but still make things interesting. If you try to metagame, you will be beaten with
-		hammers. Important Rule: Be funny! Ask funny questions, and act like a real detective.
+		When someone else is telling a story, it's your turn to ask questions. Give others room to
+		speak. Respect personal boundaries. Still make things interesting. Don't be an attention whore.
+		Don't metagame. If you try to metagame, you will be beaten with hammers. Important Rule: Be
+		funny. Act like a real detective. Get into it.
 	</p>
 	<h2 class="text-xl font-semibold text-gray-800">Voting</h2>
 	<p class="md:indent-12">
-		After the storyteller is finished telling their story, players will honestly and anonymously
-		vote if they think the story was real or fake. Again, if you try to metagame: hammers.
+		After the storyteller finishes, everyone will honestly and anonymously vote on whether the story
+		was real or fake. Once again, if you try to metagame: hammers.
 	</p>
 	<p>Scoring Reminder:</p>
 	<ul class="list-inside list-disc">
-		<li>If you guess correctly, you get 1 point.</li>
-		<li>If you guess wrong, the storyteller gets 1 point.</li>
+		<li>Guess correctly -> you get 1 point.</li>
+		<li>Guess wrong -> the storyteller gets 1 point.</li>
 		<li>Convincing people is just as valuable as detecting lies.</li>
 	</ul>
 	<h2 class="text-xl font-semibold text-gray-800">Final Reminder</h2>
